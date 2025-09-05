f287eae42ea0a97b8c2f6125ed5d66947f2209c1
io0323 <iokuma0323@gmail.com>
2025-09-05 14:28:12 +0900

    Update build configurations and dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.io.tea.android"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
